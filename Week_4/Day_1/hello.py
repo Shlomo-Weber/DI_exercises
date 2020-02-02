6642265c2c8d65c6eea9becfd5ff5f5dbf3b3b5c
Shlomo <shlomo@Vex.com>
2020-02-02 16:35:34 +0200

    Finished XP and XP gold

---
+++ Week_4/Day_1/hello.py
@@ -0,0 +1,29 @@
+# name = "shlomo"
+# num = 613
+
+# print(num)
+# print("hello " + name)
+
+# n = "herp"
+# p = "derp"
+# print(n + " " + p)
+
+# Exercise
+# phrase = " Hello World "
+# print(phrase *4)
+
+# name = "shlomo"
+# age = 24
+# shoe_size = 10.5
+# info = "I like to play with nunchucks, my name is " + name + ".I am " + str(age) +",and my shoe size is " + str(shoe_size)
+# print(info)
+
+# Exercise Gold
+
+# newphrase = " I love Python "
+
+# print(phrase*4 + newphrase*4)
+
+# x = input()
+# print(int(x) + int(x+x) + int(x+x+x) + int(x+x+x+x))
+
