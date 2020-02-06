2ab1c1dce28b5b1a347cf3643170a475117ced00
Shlomo <shlomo@Vex.com>
2020-02-06 16:56:53 +0200

    Exercises for day 5

---
+++ Week_4/Day_5/Exercises.py
@@ -0,0 +1,59 @@
+# Find max
+
+# def find_max(num_list):
+#     max_num = num_list[0]
+#     for num in num_list:
+#         if num > max_num:
+#             max_num = num
+#     return max_num
+
+# Find Sum
+
+# def find_sum(num_list):
+#     total = 0
+#     for num in num_list:
+#         total += num
+#     return total
+
+# Find Factorial
+
+# def find_fac(num):
+#     fac = num
+#     for i in range(1,num):
+#         fac *= i
+#     return fac
+
+# XP Gold day 4
+# Exercise 6
+# Favorite Book
+# def favorite_book(title):
+#     print(f"One of my favorite books is {title}")
+#
+# favorite_book("Name of the Wind")
+
+# Exercise 3
+# def describe_city(city, country="Israel"):
+#     print(f"{city} is a city in {country}")
+#
+# describe_city("Chaifa")
+# describe_city("Ashdod")
+# describe_city("Los Angeles")
+
+# Exercise 8 & 9
+# magicians = ["Froopy", "Droopy", "Herpy", "Derpy"]
+# def show_magicians():
+#     trump = make_great()
+#     for corona in trump:
+#         print(corona)
+#
+#
+#
+#
+# def make_great():
+#     new_mag = []
+#     for magician in magicians:
+#         magician += " the Great"
+#         new_mag.append(magician)
+#     return new_mag
+#
+# show_magicians()
