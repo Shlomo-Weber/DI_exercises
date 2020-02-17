00b108f8e964d920528cbb24aec91851b978d826
Shlomo <shlomo@Vex.com>
2020-02-17 10:00:58 +0200

    random stuff

--- Week_4/Day_3/lesson.py
+++ Week_4/Day_3/lesson.py
@@ -171,4 +171,4 @@
 l = [1,2,3,4,5,6]
 print(l.index(5))
 
-# Exercise 11
+#
--- Week_5/Day_1/menu_manager.py
+++ Week_5/Day_1/menu_manager.py
@@ -1,11 +1,15 @@
 class MenuManager:
     def __init__(self, menu):
         self.menu = {}
-    def add_item(self,name, price, spice, gluten):
-        self.menu[name, price, spice, gluten] = "Burger", 15, "A", True
 
-my_Menu = MenuManager("menu")
+    def add_item(self, name, price, spice_level, gluten):
+        item = {
+            "name":name,
+            "price":price,
+            "spice_level": spice_level,
+            "gluten": gluten
+        }
+        self.menu.append(item)
+my_menu = MenuManager("herp")
 
-my_Menu.add_item("Burger", 15, "A", True)
-
-print(my_Menu.menu)
+print(my_menu.menu)
